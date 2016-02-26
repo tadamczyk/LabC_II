fd9c05f90fc8af21f3981a5a6dd80855d0371624
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2016-02-26 11:07:23 +0100

    EDIT

--- lab1/zad3.c
+++ lab1/zad3.c
@@ -2,9 +2,19 @@
 //w różnych sytuacjach, od których może zależeć wynik działania.
 
 #include <stdio.h>
+void dziwne_for(int tab[], int size)
+{
+	int i;
+	for(i=0;i<size;)
+		printf("%i ",tab[i++]);
+}
 int main()
 {
-	char z;
-	for(;(z=getchar())!=EOF;printf("%c",z));
+	int tab[4]={10,11,12,13};
+	for(int i=0;i<4;i++)
+		printf("%i ",tab[i]);
+	printf("\n");
+	dziwne_for(tab,4);
+	printf("\n");
 	return 0;
 }
