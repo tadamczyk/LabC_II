8903714929750fe936ecaff3f2ff786162a74776
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2016-03-11 11:26:54 +0100

    Update

--- lab4/zad1.c
+++ lab4/zad1.c
@@ -1,7 +1,37 @@
 // Opracować zestaw operacji na liczbach zespolonych potrzebny do prawidłowego działania tego programu.
 // W tym celu należy prototypy (nagłówki) podanych tam funkcji, realizujących działania, zastąpić lub uzupełnić pełnymi definicjami działań.
 // Oczywiście dla napisania tych funkcji, potrzebne jest zastosowanie własnej wiedzy o liczbach zespolonych (np. z algebry).
+/*
+#include <stdio.h>        Standard Library of Input and Output
+#include <complex.h>      Standard Library of Complex Numbers
 
+int main() {
+
+    double complex z1 = 1.0 + 3.0 * I;
+    double complex z2 = 1.0 - 4.0 * I;
+
+    printf("Working with complex numbers:\n\v");
+
+    printf("Starting values: Z1 = %.2f + %.2fi\tZ2 = %.2f %+.2fi\n", creal(z1), cimag(z1), creal(z2), cimag(z2));
+
+    double complex sum = z1 + z2;
+    printf("The sum: Z1 + Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
+
+    double complex difference = z1 - z2;
+    printf("The difference: Z1 - Z2 = %.2f %+.2fi\n", creal(difference), cimag(difference));
+
+    double complex product = z1 * z2;
+    printf("The product: Z1 x Z2 = %.2f %+.2fi\n", creal(product), cimag(product));
+
+    double complex quotient = z1 / z2;
+    printf("The quotient: Z1 / Z2 = %.2f %+.2fi\n", creal(quotient), cimag(quotient));
+
+    double complex conjugate = conj(z1);
+    printf("The conjugate of Z1 = %.2f %+.2fi\n", creal(conjugate), cimag(conjugate));
+
+    return 0;
+}
+*/
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
